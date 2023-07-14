2deff7ab5a6ba301f1bd9ebddef65aface6401f7
OleGrim <olegrkh@gmail.com>
2023-07-14 17:20:10 +0300

    Версия 1.

--- convert.py
+++ convert.py
@@ -7,19 +7,27 @@ def convert_xml_to_md(xml_file_path, output_folder):
         xml_data = xml_file.read()
         data_dict = xmltodict.parse(xml_data)
 
-        for entry in data_dict['item']['entry']:
-            title = entry['title']
-            content = entry['content']['#text']
+        items = data_dict['item']  # Получение всех элементов <item>
 
-            # Преобразование контента в Markdown
-            md_content = markdown.markdown(content)
-
-            # Создание файла Markdown
-            md_file_path = os.path.join(output_folder, f'{title}.md')
-            with open(md_file_path, 'w', encoding='utf-8') as md_file:
-                md_file.write(md_content)
+        if isinstance(items, list):  # Если есть несколько элементов <item>
+            for item in items:
+                convert_item_to_md(item, output_folder)
+        elif isinstance(items, dict):  # Если только один элемент <item>
+            convert_item_to_md(items, output_folder)
 
     print('Преобразование завершено!')
+
+def convert_item_to_md(item, output_folder):
+    title = item['title']
+    content = item['description']
+
+    # Преобразование контента в Markdown
+    md_content = markdown.markdown(content)
+
+    # Создание файла Markdown
+    md_file_path = os.path.join(output_folder, f'{title}.md')
+    with open(md_file_path, 'w', encoding='utf-8') as md_file:
+        md_file.write(md_content)
 
 # Путь к файлу XML и папке для сохранения файлов Markdown
 xml_file_path = 'C:\Home\Python\XMLtoMD\spring_2010.xml'
